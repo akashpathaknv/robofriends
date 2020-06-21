1d724319d97c41b906783897c82fd146cdc587c8
Akash Pathak <akashpathaknv@gmail.com.com>
2020-06-21 16:25:50 +0530

    Updates

---
+++ precache-manifest.98ec122a21166e202c09ce44e51c7761.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea661f046cc8fe905b4d14a3c4cf554",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "76365a6be1627439f0f0",
+    "url": "/robofriends/static/css/main.338e0e19.chunk.css"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/robofriends/static/js/2.7ec5fd90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.7ec5fd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76365a6be1627439f0f0",
+    "url": "/robofriends/static/js/main.7c8fcda3.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
