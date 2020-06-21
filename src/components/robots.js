0b7e77c49d3908cc85166e25aefc9e41046beede
Akash Pathak <akashpathaknv@gmail.com.com>
2020-06-21 16:16:45 +0530

    initial commit

---
+++ src/components/robots.js
@@ -0,0 +1,86 @@
+export const robots = [
+	{
+		id:1,
+		name : 'Akash Pathak',
+		usernme : 'Sky',
+		email : 'akash.pathak@gmail.com',
+
+	},
+	{
+		id:2,
+		name : 'Nutan Pathak',
+		usernme : 'New',
+		email : 'nutan.pathak@gmail.com',
+
+	},
+	{
+		id:3,
+		name : 'Vimal Pathak',
+		usernme : 'Vimal',
+		email : 'vimal.pathak@gmail.com',
+
+	},
+	{
+		id:4,
+		name : 'Samay Trivedi',
+		usernme : 'Sam',
+		email : 'samaytrivedi@gmail.com',
+
+	},
+	{
+		id:5,
+		name : 'Mansi Pandya',
+		usernme : 'Sis',
+		email : 'mansi.pandya@gmail.com',
+
+	},
+	{
+		id:6,
+		name : 'Karan Pandya',
+		usernme : 'karan',
+		email : 'karanpandya@gmail.com',
+
+	},
+	{
+		id:7,
+		name : 'moksh dave',
+		usernme : 'moku',
+		email : 'mokshdave@gmail.com',
+
+	},
+	{
+		id:8,
+		name : 'Meena Trivedi',
+		usernme : 'Moti',
+		email : 'meenatrivedi@gmail.com',
+
+	},
+	{
+		id:9,
+		name : 'Chandresh Trivedi',
+		usernme : 'lala',
+		email : 'chandreshtrivedi@gmail.com',
+
+	},
+	{
+		id:10,
+		name : 'Vibha trivedi',
+		usernme : 'Vibha',
+		email : 'vibhatrivedi@gmail.com',
+
+	},
+	{
+		id:11,
+		name : 'Rashi trivedi',
+		usernme : 'Rotlu',
+		email : 'rashitrivedi@gmail.com',
+
+	},
+	{
+		id:12,
+		name : 'Aashka dave',
+		usernme : 'jadi',
+		email : 'aashkadave@gmail.com',
+
+	}
+]
